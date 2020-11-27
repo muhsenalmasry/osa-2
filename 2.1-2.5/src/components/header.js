151a9a08935feecb60c651973e922014ea4654bc
muhsenalmasry <74643833+muhsenalmasry@users.noreply.github.com>
2020-11-27 20:12:18 +0200

    Add files via upload

---
+++ 2.1-2.5/src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = (props) => (
+  <div>
+    <h2>{props.title}</h2>
+  </div>
+)
+
+export default Header
---
+++ 2.1-2.5/src/components/total.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Total = ({parts}) => {
+    
+    const total = parts.reduce((s,{exercises}) => s+exercises,0)
+    return(
+        <h3>
+            total of {total} exercises
+        </h3>
+        
+    )
+}
+
+export default Total
